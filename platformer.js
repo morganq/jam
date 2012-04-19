1e45939a4998d0054f18ba28d2a9d8ae6e63dd90
unknown <morgan@.(none)>
2012-04-18 23:00:00 -0400

    Updates to the platformer sample game

--- platformer.js
+++ platformer.js
@@ -17,13 +17,60 @@ window.onload = function(){
 initialize = function(){
 	var game = jam.Game(640, 480, document.body);
 	
-	jam.Debug.showBoundingBoxes = true;		// Uncomment to see the collision regions
+	//jam.Debug.showBoundingBoxes = true;		// Uncomment to see the collision regions
+	
+	var bg = jam.Sprite(0, 0);
+	bg.width = 640; bg.height = 480;
+	bg.image = document.createElement("canvas");
+	bg.image.width = 640;
+	bg.image.height = 480;
+	erase = false;
+	var ctx = bg.image.getContext("2d");
 	
 	var map = jam.LevelMap.loadTileMap(32, map1, "platformer_data/tiles.png");
 	var player = makePlayer(game, map);
 	
+	bg.color = "rgba(0,128,255,0.75)";
+	
+	bg.update = jam.extend(bg.update, function(elapsed){
+		for(var i = 0; i < 7; i++)
+		{
+			ctx.beginPath();
+			if(erase){
+				ctx.arc(player.smoothX + Math.random() * 20 - 10, player.smoothY + Math.random() * 20 - 10, Math.random() * 3 + 2, 0, 2 * Math.PI, false);
+				ctx.fillStyle = "rgba(255,255,255, 0.75)";
+			}
+			else{
+				ctx.arc(player.smoothX + Math.random() * 16 - 8, player.smoothY + Math.random() * 16 - 8, Math.random() * 3 + 1, 0, 2 * Math.PI, false);
+				ctx.fillStyle = bg.color;	
+			}
+			ctx.fill();
+		}
+		ctx.beginPath();
+		ctx.arc(player.smoothX, player.smoothY, 4, 0, 2 * Math.PI, false);
+		ctx.fillStyle = erase ? "rgba(255,255,255, 0.75)" : bg.color;		
+		ctx.fill();
+		
+		if(jam.Input.justPressed("B")){
+			erase = false;
+		}
+		if(jam.Input.justPressed("E")){
+			erase = true;
+		}
+		if(jam.Input.justPressed("1")){
+			bg.color = "rgba(0,128,255,0.75)";
+		}
+		if(jam.Input.justPressed("2")){
+			bg.color = "rgba(255,0,64,0.75)";
+		}
+		if(jam.Input.justPressed("3")){
+			bg.color = "rgba(0,0,0,0.75)";
+		}
+	});	
+	
 	game.add(map);	
 	game.add(player);
+	game.add(bg);
 	
 	game.run();
 }
@@ -40,6 +87,9 @@ makePlayer = function(game, map){
 	player.setCollisionOffsets(6, 0, 20, 31);
 	player.setLayer(1);
 
+	player.lastPositions = [];
+	player.smoothX = player.x + player.width/2;
+	player.smoothY = player.y + player.height/2;
 	
 	player.update = jam.extend(player.update, function(elapsed){
 		// Collision
@@ -73,6 +123,14 @@ makePlayer = function(game, map){
 		
 		// Gravity
 		player.velocity.y += 400 * elapsed;
+		
+		player.lastPositions.splice(0,0, jam.Vector(player.x + player.width / 2, player.y + player.height / 2));
+		for(var i = 0; i < Math.min(player.lastPositions.length, 10); ++i){
+			player.smoothX += player.lastPositions[i].x;
+			player.smoothY += player.lastPositions[i].y;
+		}
+		player.smoothX /= i+1;
+		player.smoothY /= i+1;
 	});
 	return player;
 }
