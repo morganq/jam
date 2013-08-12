b5a95b5ab49eb29153b1756a69f4ca615f6c7d4b
Ingrid <hi@ingridcheung.com>
2013-08-11 22:56:06 -0400

    Refactored game.

--- jam/game.js
+++ jam/game.js
@@ -1,113 +1,111 @@
 define(["vector", "sprite", "input", "util"], function(Vector, Sprite, Input, Util) {
-	return function(width, height, parentElement, zoom){
-		var self = {};
+  var game = function(width, height, parentElement, zoom){
+	this._canvas = document.createElement("canvas");
+	// Style attributes for doing "pixel-zoom" (nearest neighbor).
+	this._canvas.setAttribute("style","image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering:-webkit-optimize-contrast; -ms-interpolation-mode:nearest-neighbor;");
+	this._canvas.style.position = "relative";
+	this._canvas.style.border = "1px solid black";
+	this._context = this._canvas.getContext("2d");
+	this.hasFocus = false;
 
-		self._canvas = document.createElement("canvas");
-		// style attributes for doing "pixel-zoom" (nearest neighbor)
-		self._canvas.setAttribute("style","image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering:-webkit-optimize-contrast; -ms-interpolation-mode:nearest-neighbor;");
-		self._canvas.style.position = "relative";
-		self._canvas.style.border = "1px solid black";
-		self._context = self._canvas.getContext("2d");
-		self.hasFocus = false;
+	zoom = zoom || 1;
 
-		zoom = zoom || 1;
+	this.root = new Sprite(0,0);
+	this.root.scene = new Sprite(0, 0);
+	this.root.add(this.root.scene);
+	this.root.ui = new Sprite(0, 0);
+	this.root.add(this.root.ui);
 
-		self.root = new Sprite(0,0);
-		self.root.scene = new Sprite(0, 0);
-		self.root.add(self.root.scene);
-		self.root.ui = new Sprite(0, 0);
-		self.root.add(self.root.ui);
+	if(parentElement === undefined) {
+	  parentElement = document.body;
+	}
 
-		if(parentElement === undefined) {
-			parentElement = document.body;
-		}
+	this.width = width;
+	this.height = height;
+	this._canvas.width = width;
+	this._canvas.height = height;
+	this._context.width = this.width;
+	this._context.height = this.height;
+	this.zoom = zoom;
 
-		self.width = width;
-		self.height = height;
-		self._canvas.width = width;
-		self._canvas.height = height;
-		self._context.width = self.width;
-		self._context.height = self.height;
-		self.zoom = zoom;
+	// Always keep the canvas in the middle of the parent element
+	var onresize = function(){
+	  this._canvas.style.left = (parentElement.clientWidth / 2 - (this.width * this.zoom) / 2) + "px";
+	  this._canvas.style.top = (parentElement.clientHeight / 2 - (this.height * this.zoom) / 2) + "px";
+	  this._canvas.style.width = (this.width * this.zoom) + "px";
+	  this._canvas.style.height = (this.height * this.zoom) + "px";
+	}.bind(this);
+	onresize();
+	parentElement.onresize = onresize;
 
-		// Always keep the canvas in the middle of the parent element
-		var onresize = function(){
-			self._canvas.style.left = (parentElement.clientWidth / 2 - (self.width * self.zoom) / 2) + "px";
-			self._canvas.style.top = (parentElement.clientHeight / 2 - (self.height * self.zoom) / 2) + "px";
-			self._canvas.style.width = (self.width * self.zoom) + "px";
-			self._canvas.style.height = (self.height * self.zoom) + "px";
-		};
-		onresize();
-		parentElement.onresize = onresize;
+	// Timing
+	this.fps = 50;
+	this.elapsed = 0;	// Time since the last frame in seconds
+	this.time = 0;
+	this.lastFrameTime = new Date().getTime();
 
-		// Timing
-		self.fps = 50;
-		self.elapsed = 0;	// Time since the last frame in seconds
-		self.time = 0;
-		var lastFrameTime = new Date().getTime();
-		var startTime = lastFrameTime;
+	this.bgColor = "rgb(255,255,255)";
 
-		self.bgColor = "rgb(255,255,255)";
+	// If they didn't supply this argument, assume the doc body
+	// as the parent element for the canvas
+	if(parentElement === undefined || parentElement === null){
+	  parentElement = document.body;
+	}
+	parentElement.appendChild(this._canvas);
 
-		// If they didn't supply this argument, assume the doc body
-		// as the parent element for the canvas
-		if(parentElement === undefined || parentElement === null){
-			parentElement = document.body;
-		}
-		parentElement.appendChild(self._canvas);
+	this._tick = function(){
+	  this.update();
+	  this.render();
+	  window.setTimeout(this._tick, 1000.0/this.fps);
+    }.bind(this);
 
-		self._tick = function(){
-			self.update();
-			self.render();
-			window.setTimeout(self._tick, 1000.0/self.fps);
-		};
+	Util.mixinOn(this);
+  };
 
-		// Called every frame to do all logic unrelated to rendering. Calls
-		// update on the root sprite which will recursively call update on its
-		// children.
-		self.update = function(){
-			currentTime = new Date().getTime();
-			self.elapsed = (currentTime - lastFrameTime) / 1000.0;
-			lastFrameTime = currentTime;
+  // Called every frame to do all logic unrelated to rendering. Calls
+  // update on the root sprite which will recursively call update on its
+  // children.
+  game.prototype.update = function(){
+	var currentTime = new Date().getTime();
+	var startTime = this.lastFrameTime;
+	this.elapsed = (currentTime - this.lastFrameTime) / 1000.0;
+	this.lastFrameTime = currentTime;
 
-			self.time = (currentTime - startTime) / 1000.0;
+	this.time = (currentTime - startTime) / 1000.0;
 
-			if(self.hasFocus) {
-				self.root.update(self.elapsed);
-				Input.update(self);
-			}
-		};
+	if(this.hasFocus) {
+	  this.root.update(this.elapsed);
+	  Input.update(this);
+	}
+  };
 
-		// Called every frame. Clears the screen, then does a recursive render
-		// call on the root sprite. This will call render on its children and
-		// so on.
-		self.render = function(){
-			var ctx = self._context;
-			ctx.save();
-			ctx.fillStyle = self.bgColor;
-			ctx.fillRect(0,0,self.width,self.height);
+  // Called every frame. Clears the screen, then does a recursive render
+  // call on the root sprite. This will call render on its children and
+  // so on.
+  game.prototype.render = function(){
+	var ctx = this._context;
+	ctx.save();
+	ctx.fillStyle = this.bgColor;
+	ctx.fillRect(0,0,this.width,this.height);
 
-			self.root.render(ctx);
-			ctx.restore();
-		};
+	this.root.render(ctx);
+	ctx.restore();
+  };
 
-		self.run = function(){
-			self._tick();
-			Input.registerGame(self);
-		};
+  game.prototype.run = function(){
+	this._tick();
+	Input.registerGame(this);
+  };
 
-		self.gainFocus = function() {
-			self._canvas.style.outline = "2px solid #888";
-			self.hasFocus = true;
-		}
+  game.prototype.gainFocus = function() {
+	this._canvas.style.outline = "2px solid #888";
+	this.hasFocus = true;
+  };
 
-		self.loseFocus = function() {
-			self._canvas.style.outline = "";
-			self.hasFocus = false;
-		}
+  game.prototype.loseFocus = function() {
+	this._canvas.style.outline = "";
+	this.hasFocus = false;
+  };
 
-		Util.mixinOn(self);
-
-		return self;
-	};
+  return game;
 });
